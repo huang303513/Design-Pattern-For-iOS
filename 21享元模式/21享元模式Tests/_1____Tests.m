b0791e8799ab2ff5e30ff201ada139db80e9a3d0
黄成都 <394042583@qq.com>
2015-08-15 17:41:40 +0800

    享元模式没有完成

---
+++ 21享元模式/21享元模式Tests/_1____Tests.m
@@ -0,0 +1,40 @@
+//
+//  _1____Tests.m
+//  21享元模式Tests
+//
+//  Created by yifan on 15/8/15.
+//  Copyright (c) 2015年 黄成都. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface _1____Tests : XCTestCase
+
+@end
+
+@implementation _1____Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
