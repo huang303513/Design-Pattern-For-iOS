a3e1673657975e669078fa4ae65918081107a43f
黄成都 <394042583@qq.com>
2015-08-15 10:19:37 +0800

    桥接模式未完成

---
+++ 17桥接模式/17桥接模式/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  17桥接模式
+//
+//  Created by yifan on 15/8/15.
+//  Copyright (c) 2015年 黄成都. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
