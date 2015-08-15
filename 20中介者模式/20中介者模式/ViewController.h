619819b69e440b262b70bd4963ada8dec848af80
黄成都 <394042583@qq.com>
2015-08-15 17:41:04 +0800

    中介者模式没有完成

---
+++ 20中介者模式/20中介者模式/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20中介者模式
+//
+//  Created by yifan on 15/8/15.
+//  Copyright (c) 2015年 黄成都. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
